e1c50446585e6fab33f21bbb9819f6b897933cc6
SYSINF\adai104 <murier.simon@gmail.com>
2019-01-17 11:16:54 +0100

    Reorganize Lib into folders

---
+++ KritterLib/Critter/Critter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KritterLib
+{
+    public abstract class Critter
+    {
+        protected String m_latinName;
+        protected String m_name;
+        protected Gender m_sex;
+
+        public abstract void yell();
+        public void exposeID()
+        {
+            Console.WriteLine("Latin name : " + this.m_latinName);
+            Console.WriteLine("Name : " + this.m_name);
+            Console.WriteLine("Gender : " + this.m_sex);
+        }
+    }
+}
